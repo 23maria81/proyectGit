84c018f400cc2179f542c3ef354f466ceb56361f
23maria81 <mariabastardo81@hotmail.com>
2016-09-19 14:31:47 +0200

    crearPreguntas

---
+++ crearPreguntas.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>pregunta</title>
+</head>
+<body>
+    <h1>formulario</h1>
+    <form method="get" action="crearPreguntas.php">
+        <input type="text" name="pregunta">
+        <input type="text" name="R1" placeholder="Respuesta 1">
+        <input type="text" name="R2" placeholder="Respuesta 2">
+        <input type="text" name="R3" placeholder="Respuesta 3">
+        <input type="text" name="R4" placeholder="Respuesta 4">
+        <input type="submit" value="mandar">
+
+    </form>
+
+    <?php
+
+  
+    ?>
+
+</body>
+</html>
