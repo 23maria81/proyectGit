bc57a1b6e57d775ca4778bf6f72ad1d73f5bbc96
23maria81 <mariabastardo81@hotmail.com>
2016-09-20 13:14:57 +0200

    crearPreguntas

--- crearPreguntas.php
+++ crearPreguntas.php
@@ -5,21 +5,22 @@
     <title>formularios</title>
 </head>
 <body>
-    <h1></h1>
+    <h1>Agregar informacion a la base de datos</h1>
 
-    <form method="get" action="crearPreguntas.php">
-        <input type="text" name="pregunta">
-        <input type="text" name="R1" placeholder="Respuesta 1"><br>
-        <input type="text" name="R2" placeholder="Respuesta 2">
-        <input type="text" name="R3" placeholder="Respuesta 3">
-        <input type="text" name="R4" placeholder="Respuesta 4">
-        <input type="submit" value="mandar">
-
+    <form method="post" action="install.php">
+        temas:<input type="Text" name="tema"><br><br>
+        preguntas:<input type="Text" name="pregunta"><br><br>
+        respuestas:<input type="Text" name="respuesta"><br><br>
+        <input type="Submit" name="enviar" value="Agregar respuestas">
     </form>
 
     <?php
-
-
+      $conexion = mysql_connect("localhost", "root");
+      mysql_select_db("mydb",$e);
+      $sql = "INSERT INTO install (preguntas, respuestas, temas)" +
+      "VALUES ('$tema', '$pregunta', '$respuesta')";
+      $result = mysql_query($sql);
+      echo "¡Gracias! Hemos recibido sus datos.\n"
     ?>
 
 </body>
--- index.php
+++ index.php
@@ -2,8 +2,7 @@
   require 'vendor/autoload.php';
   $app = new \Slim\App();
   $app->get("/", function ($request, $response, $args) {
-    $response->write("<h1> Markdown:<br> Herramienta de conversion de texto plano a HTML
-                      Es un lenguaje de marcado que facilita la escritura </h1>");
+    $response->write("<h1>Formularios</h1>");
 });
 $app->run();
 ?>
