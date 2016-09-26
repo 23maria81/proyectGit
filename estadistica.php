ccbf5344b9e40b4a553f18d7e4ff6787678570b7
23maria81 <mariabastardo81@hotmail.com>
2016-09-26 14:14:48 +0200

    nuevas actualizaciones

--- estadistica.php
+++ estadistica.php
@@ -1,8 +1,10 @@
 <?php
   class Estadistica{
     public function __invoke($request, $response, $next){
+        $conexion = new PDO("mysql:host=localhost;dbname=install", "root");
+        $sql = "insert into estadistica(contador) value(10);";
 
-
+        $conexion->exec($sql);
 
     return $next($request, $response);
     }
